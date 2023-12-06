8db75f3212dd916707c3761445abcefd53a658b2
Emanuel José <emanuel.jsa21@gmail.com>
2023-12-06 16:18:26 -0300

    fix

--- day04_Scratchcards/part02.py
+++ day04_Scratchcards/part02.py
@@ -1,12 +1,12 @@
-input_data = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
+# input_data = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+# Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+# Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+# Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+# Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+# Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
 
-# with open('F:\\Projects\\AdventOfCode\\day04_Scratchcards\\data.txt', 'r') as file:
-#     input_data = file.read()
+with open('data.txt', 'r') as file:
+    input_data = file.read()
 
 lines = input_data.splitlines()
 lines_cards = [line.split(":")[1].strip() for line in lines]
@@ -14,11 +14,11 @@ game_cards = [card.split("|") for card in lines_cards]
 cards = [[list(filter(None, item.strip().split(" "))) for item in card] for card in game_cards]
 
 def calculate_with_extra(input): 
-    cards_count = [1] * len(input)
+    cards = [1] * len(input)
 
     for (i, card) in enumerate(input):
         for j in range(i + 1, i + 1 + len(set(card[0]) & set(card[1]))):
-            cards_count[j] += 1 * cards_count[i]
+            cards[j] += 1 * cards[i]
 
     return sum(cards)
 
