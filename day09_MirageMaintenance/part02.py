365b274384d4255003d36532e8c22c135b666d40
Emanuel José <emanuel.jsa21@gmail.com>
2023-12-09 13:57:54 -0300

    finish day 09

---
+++ day09_MirageMaintenance/part02.py
@@ -0,0 +1,22 @@
+# input_data = """10  13  16  21  30  45"""
+
+with open('data.txt', 'r') as file:
+    input_data = file.read()
+
+lines = input_data.split("\n")
+
+values = []
+
+def get_final_value(numbers):
+    diffs = [numbers[i+1] - numbers[i] for i in range(len(numbers)-1)]
+    if all(v == 0 for v in diffs):
+        return numbers[-1] + diffs[-1]
+    else:
+        return numbers[0] - get_final_value(diffs)
+        
+for line in lines:
+    numbers = [int(n) for n in line.split()]
+    value = get_final_value(numbers)
+    values.append(value)
+            
+print(sum(values))
